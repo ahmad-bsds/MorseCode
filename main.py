19ff80fdb4d76d23edd39be2ca8e64bdca731a16
MUHAMMAD AHMAD <ahmad.bsds@gmail.com>
2023-08-13 11:26:09 +0500

    Modify and implement a condition for dict vals.

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ morse_code_dict = {
 
 # Getting a string.
 string: str = input("Enter a string to convert: ").upper()
-
 # Converting string to morse code.
 def convert_to_morse(string: str):
     for chr in string:
-        convert_with = morse_code_dict[chr] # morse code of that specific char in loop.
-        string = string.replace(chr, convert_with) # replacing with morse code.
+        if chr in morse_code_dict.keys():
+            convert_with = morse_code_dict[chr] # morse code of that specific char in loop.
+            string = string.replace(chr, convert_with) # replacing with morse code.
     return string
 
 # Printing morse code of string.
